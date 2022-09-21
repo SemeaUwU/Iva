1d434a2f099e460d143e61e35f2bd2e52b05fb87
Juan Iturriaga <113060652+SemeaUwU@users.noreply.github.com>
2022-09-21 17:30:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Juan Iturriaga 4B
+
+a = input("Ingrese el nombre del producto: ")
+b = int(input("Ingrese el precio del producto: "))
+c = int(input("Ingrese las Unidades a llevar del producto: "))
+st = b*c
+IVA = (b*c)*0.19
+pt = IVA+b*c
+
+print("\n Producto : ", a, "\n", "Precio : ", b, "$", "\n", "Cantidad: ", c, "\n", "Subtotal: ", st,"$", "\n", "IVA: ", IVA ,"$", "\n", "Total: ", pt ,"$",)
